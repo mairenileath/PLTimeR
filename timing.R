5331c3ebf54101b56e1783d1b539de95c07aac74
mairenileath <61795083+mairenileath@users.noreply.github.com>
2021-06-03 16:17:28 +0100

    Update timing.R

--- timing.R
+++ timing.R
@@ -20,16 +20,21 @@ library(reshape2)
 
 source(paste0(r_path,"01_prepare_subclones_for_timing.R"))
 source(paste0(r_path,"02_01_identify_enriched_regions.R"))
+source(paste0(r_path,"02_02_fdr_summary_function.R"))
+source(paste0(r_path,"03_prepare_enriched_regions_for_ordering.R"))
 
 hg_genome <- read.table(genome_path,header = TRUE)
 chr_lengths = hg_genome$end
-refsegs_dir = paste0(output_dir, "refsegs/")
-landscape_dir = paste0(output_dir, "/cnlandscape/")
-allsegs_file = paste0(output_dir, tumour_type, "_allsegs.txt")
+refsegs_dir = paste0(output_dir,"refsegs/")
+landscape_dir = paste0(output_dir,"/cnlandscape/")
 gain_dir = paste0(output_dir, "gain/")
 hd_dir = paste0(output_dir,"hd/")
 loh_dir = paste0(output_dir,"loh/")
+pvals_dir = paste0(output_dir,"pvals/")
+enriched_dir = paste0(output_dir,"enriched/")
+merged_dir = paste0(output_dir,"merged/") 
 annotated_segments_file = paste0(output_dir, tumour_type,"_annotated_segments.txt")
+allsegs_file = paste0(output_dir, tumour_type, "_allsegs.txt")
 
 #Collate the subclones data from Battenberg into a single file and add the ploidy of the sample.
 # Inputs: subclone files
@@ -57,5 +62,32 @@ plot_CN_landscape(annotated_segments_file, refsegs_dir, tumour_type, chr_lengths
 # Inputs: annotated_segments file and refsegs files
 # Outputs: simulations for gain, hd and loh
 identify_enriched_regions(annotated_segments_file, refsegs_dir, gain_dir, loh_dir, hd_dir, tumour_type, chr_lengths, run)
+
+#Identify enriched events using the p-values
+# Inputs: refsegs and simulations
+# Outputs: files with enriched regions, the Bonferroni and FDR-corrected p-values
+fdr_summary(refsegs_dir, gain_dir, tumour_type, "Gain", pvals_dir)
+fdr_summary(refsegs_dir, loh_dir, tumour_type, "LOH", pvals_dir)
+fdr_summary(refsegs_dir, hd_dir, tumour_type, "HD", pvals_dir)
+
+#Remove artefacts from the enriched regions, eg. segments near telomere, centromere, HLA region, in a few samples
+# Inputs: 
+# Outputs: Enriched region files for LOH, HD, Gain, trimmed of artesfacts
+remove_artefacts(annotated_segments_file, tumour_type, enriched_dir, pvals_dir, genome_path, 10000, 3)
+
+#Merge segments overlapping the enriched regions
+# Inputs: annotated segments file and enriched region files
+# Outputs: merged enriched segments and the plots of the enriched regions are output
+merge_enriched_regions(annotated_segments_file, tumour_type, enriched_dir, genome_path, enriched_dir)
+
+#Check if any new breakpoints should be added to the enriched regions
+# Inputs: enriched region files
+# Outputs: merged enriched regions (separate files for LOH, HD and gain) and plots of the segments overlapping the enriched regions
+multipcf_new_breakpoints(output_dir, tumour_type, enriched_dir)
+
+#Process the enriched data into the format required for the ordering step with the Plackett-Luce model
+# Inputs: annotated segments file and enriched region files
+# Outputs: files with enriched regions (separate files for LOH, HD and gain) in the format for the ordering script
+prepare_ordering_data(annotated_segments_file, tumour_type, enriched_dir, genome_path, merged_dir)
 
 }
