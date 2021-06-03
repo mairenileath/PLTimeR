adda75f14f2dcd0547fe83f02f0343d34957c896
mairenileath <61795083+mairenileath@users.noreply.github.com>
2021-06-03 16:48:58 +0100

    Add clusterVisualize function

--- 05_distance_summary_functions.R
+++ 05_distance_summary_functions.R
@@ -48,16 +48,24 @@ for (i in colnames(data)) {
 }
 
 write.table(df_distances, file = paste0(cohort_name,"_distance_summary.txt"), sep="\t",quote=F, row.names = FALSE)
-# 
-# #Converts to matrix
-# df_mat <- acast(df_distances, A~B, value.var="Distance", mean)
-# df_dist <- as.dist(df_mat)
-# 
-# #Hierarchical clustering
-# hclust_result <- hclust(df_dist, method = 'average')
-# 
-# hier_clust_plot <- ggdendrogram(hclust_result, rotate = FALSE, size = 2)
-# 
-# save_plot(paste0(cohort_name,"_Hierarchical_Clustering_Plot.png"), plot = hier_clust_plot)
 }
 
+clusterVisualize = function(cohort_name) {
+
+  library(ggdendro)
+  source("functions_subtyping.R")
+
+  #Import data
+  df_distances <- read.csv(paste0(cohort_name,"_distance_summary.txt"), sep = "\t", header = TRUE, row.names = NULL)
+
+  #Converts to matrix
+  df_mat <- acast(df_distances, A~B, value.var="Distance", mean)
+  df_dist <- as.dist(df_mat)
+
+  #Hierarchical clustering
+  hclust_result <- hclust(df_dist, method = 'average')
+
+  hier_clust_plot <- ggdendrogram(hclust_result, rotate = FALSE, size = 2)
+  hier_clust_plot
+  ggsave(paste0(cohort_name,"_Hierarchical_Clustering_Plot.pdf"), plot = hier_clust_plot)
+}
